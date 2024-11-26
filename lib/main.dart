6e090529d2fc919dd3884e7f2039b143c43b816d
Ahmed Mahdaly <ahmed.mahdaly86@gmail.com>
2024-11-26 20:50:02 +0200

    package core

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'dart:io';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mr_coffee/mr_coffee_app.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,10 +10,10 @@ void main() async {
   Platform.isAndroid
       ? await Firebase.initializeApp(
           options: const FirebaseOptions(
-            apiKey: "AIzaSyAVlPfVXzdGgUL44kMbUVOC-1K1jCDsrUQ",
-            appId: "1:57181686346:android:037a36f79eec5f17a274b3",
-            messagingSenderId: "57181686346",
-            projectId: "mr-coffee-84744",
+            apiKey: 'AIzaSyAVlPfVXzdGgUL44kMbUVOC-1K1jCDsrUQ',
+            appId: '1:57181686346:android:037a36f79eec5f17a274b3',
+            messagingSenderId: '57181686346',
+            projectId: 'mr-coffee-84744',
           ),
         )
       : await Firebase.initializeApp();
